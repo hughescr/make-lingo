ff356798f5c43810a7be471a1629bf33669d7127
Craig R. Hughes <craig.git@rungie.com>
2020-04-18 12:09:02 -0700

    Convert to serverless

--- index.js
+++ index.js
@@ -4,33 +4,9 @@ const _ = require('lodash');
 const { 'default': Sheets } = require('node-sheets');
 const weighted = require('weighted');
 
-const nconf = require('nconf');
-
-// Read defaults
-nconf.argv(require('yargs') // eslint-disable-line node/no-extraneous-require
-        .wrap(null)
-        .help('help')
-        .alias('help', 'h')
-        .example('node index.js -n 10', 'Generate 10 random words with varying lengths')
-        .example('node index.js -n 20 -s 3', 'Generate 20 random words each of which has 3 syllables')
-        .options({
-            numWords: {
-                alias: ['n'],
-                describe: 'The number of words to generate',
-                parseValues: true,
-                'default': 1,
-            },
-            forceSyllables: {
-                alias: ['s', 'numSyllables'],
-                describe: 'Force all words to use this syllable count (optional: if not specified, use distribution of syllable counts',
-                parseValues: true,
-            },
-        })
-        .required('numWords'));
-
-async function loadData() {
-    const sheet = new Sheets('1kgbgBHRPOegL_fpQMn37UEsMk0h3OXfSbutV8UJZtYw');
-    await sheet.authorizeApiKey('AIzaSyCp94EJsdc1J-vDwEuW_PGeQekPL8o9k-0');
+const dataPromise = (async function loadData() {
+    const sheet = new Sheets(process.env.SHEET_ID || '1kgbgBHRPOegL_fpQMn37UEsMk0h3OXfSbutV8UJZtYw');
+    await sheet.authorizeApiKey(process.env.API_KEY || 'AIzaSyCp94EJsdc1J-vDwEuW_PGeQekPL8o9k-0');
     const syllableFrequencyData =  _.chain((await sheet.tables('A:B')).rows)
         .filter(row => row.Frequency && row.Frequency.value &&
                         row.Syllable && row.Syllable.value)
@@ -48,16 +24,20 @@ async function loadData() {
     const syllableCountFrequencies = _.map(syllablePerWordData, 'Frequency');
 
     return { syllables, frequencies, syllableCounts, syllableCountFrequencies };
-}
+})();
 
-(async () => {
-    const { syllables, frequencies, syllableCounts, syllableCountFrequencies } = await loadData();
-    for(let i = 0; i < nconf.get('numWords'); i++) {
-        const syllables_for_this_word = nconf.get('numSyllables') || weighted.select(syllableCounts, syllableCountFrequencies);
+module.exports.makeLingo = async (event) => {
+    const { syllables, frequencies, syllableCounts, syllableCountFrequencies } = await dataPromise;
+
+    const words = [];
+    for(let i = 0; i < (event.queryStringParameters && event.queryStringParameters.words || 100); i++) {
+        const syllables_for_this_word = event.queryStringParameters && event.queryStringParameters.syllables || weighted.select(syllableCounts, syllableCountFrequencies);
         let word = '';
         for(let s = 0; s < syllables_for_this_word; s++) {
             word = `${word}${weighted.select(syllables, frequencies)}`;
         }
-        console.log(word);
+        words.push(word);
     }
-})();
+
+    return words;
+};
